9428cfa44fd248bbb7b345148753b82ed9c1536e
mabus <m.buscaroli@outlook.com>
2022-04-12 13:23:52 +0100

    added a StyledHamburgerMenu component in order to pass the animation properties down to the fonttawesome icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,18 @@ import React from 'react'
 import styled, { css } from 'styled-components/macro'
 import { Link } from 'react-router-dom'
 import { menuLinks } from '../data/menuLinks'
+import { FaBars } from 'react-icons/fa'
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 80px;
-  background-color: red;
+  background-color: teal;
 `
 
 const Logo = styled(Link)`
-  color: silver;
+  color: white;
   font-size: 3rem;
   font-weight: 300;
   text-decoration: none;
@@ -23,6 +24,10 @@ const NavMenu = styled.div`
   display: flex;
   width: 40%;
   justify-content: space-evenly;
+
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
 `
 
 const NavMenuLink = styled(Link)`
@@ -31,6 +36,26 @@ const NavMenuLink = styled(Link)`
   font-weight: 500;
   letter-spacing: 1.1px;
   text-decoration: none;
+`
+
+const HamburgerWrapper = styled.div`
+  color: white;
+  font-size: 1.5rem;
+  padding-right: 1rem;
+  display: none;
+  cursor: pointer;
+
+  @media screen and (max-width: 500px) {
+    display: block;
+  }
+`
+
+const StyledHamburgerMenu = styled(FaBars)`
+  transition: all, 0.3s;
+
+  &:hover {
+    transform: scale(1.5);
+  }
 `
 
 function Navbar() {
@@ -46,6 +71,9 @@ function Navbar() {
           )
         })}
       </NavMenu>
+      <HamburgerWrapper>
+        <StyledHamburgerMenu icon={FaBars} />
+      </HamburgerWrapper>
     </Nav>
   )
 }
